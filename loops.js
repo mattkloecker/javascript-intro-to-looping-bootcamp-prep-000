6c910dc6d43de847e1faa88340023457ac2432c0
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-08-28 20:16:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,6 +10,9 @@ function forLoop(array){
     return array
 }
 
-function whileLoop(n){
-  return "done"
+function whileLoop(number){
+  while (number > 0) {
+    console.log(number--);  
+  }
+  return 'done';
 }
